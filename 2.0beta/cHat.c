08c0d63181405648a70b8a60342524ff6f67bab4
mofarrell <micpof@gmail.com>
2013-11-17 04:02:38 -0500

    Added some initialization for ncurses. Should be moved to a display file.

--- 2.0beta/cHat.c
+++ 2.0beta/cHat.c
@@ -2,9 +2,10 @@
  * The main file for the cHat chat client.
  */
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> /* printf */
+#include <stdlib.h> /* atoi */
 #include <getopt.h>
+#include <curses.h>
 
 void initialize_video(void) {
     printf("Haha Not implemented\n");
@@ -56,6 +57,20 @@ int main(int argc, char *argv[]) {
     if (ip_address)
         printf("ip_address: %s\n", ip_address);
     printf("port: %d\n", port);
+
+    WINDOW *main_window;
+    if ((main_window = initscr()) == NULL){
+        fprintf(stderr, "ncurses failed.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    keypad(main_window, TRUE);  /* keyboard mapping. */
+    nodelay(main_window, TRUE);  /* refresh. */
+    (void) nonl();  /* tell curses not to do NL->CR/NL on output. */
+    (void) cbreak();  /* don't wait for line break. */
+    (void) noecho();  /* don't print getch to stdout. */
+
+    
 
     return 0;
 }
