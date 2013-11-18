71a548b43249abea1df96e72ca1066b30e11f489
mofarrell <micpof@gmail.com>
2013-11-18 16:59:24 -0500

    Cleaned up and made small changes and fixes to display and network.

--- pre-alpha/src/network.c
+++ pre-alpha/src/network.c
@@ -1,5 +1,17 @@
+/* network.c
+ */
 #include "network.h"
 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h> /* free */
+
+/* initialize_network:
+ * This initializes all network systems.  It called to start the network
+ * thread.
+ */
 void initialize_network(void *network_settings){
     int port = ((struct network_data *)network_settings)->port;
     (void) port; /* for now */
--- pre-alpha/src/network.h
+++ pre-alpha/src/network.h
@@ -1,14 +1,8 @@
-/* network.h*
+/* network.h
  */
 
 #ifndef _CHAT_NETWORK_H_
 #define _CHAT_NETWORK_H_
-
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <stdio.h>
-#include <stdlib.h> /* free */
 
 struct network_data {
     int port;
